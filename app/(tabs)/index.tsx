224b81cbaa2c2ffdce8295842fd760fb43f9c996
Cezar Derevlean <cezar.derevlean@gmail.com>
2024-04-20 01:22:27 +0300

    big and image button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 import { Link } from "expo-router";
-import { View } from "tamagui";
+import { Image, View } from "tamagui";
 import { Button, ButtonNext, JourneyStep } from "@/components";
+import { StarFull } from "@tamagui/lucide-icons";
+// import image1 from "./assets/images/helping-hands.png";
 
 export default function TabOneScreen() {
   return (
@@ -10,6 +12,28 @@ export default function TabOneScreen() {
       </Link>
       <Button mt="$6">Copaci</Button>
       <JourneyStep mt="$6" />
+      <Button mt="$6" size="$9" borderBottomWidth={7}>
+        <StarFull size="$4" /> Copaci
+      </Button>
+
+      <Button
+        mt="$6"
+        size="$9"
+        maxWidth={300}
+        height={150}
+        borderBottomWidth={7}
+        overflow="hidden"
+        backgroundColor="$accentColor"
+        borderRadius="$8"
+      >
+        <Image
+          width={296}
+          height="100%"
+          borderRadius="$7"
+          source={require("../../assets/images/helping-hands.jpg")}
+          resizeMode="stretch"
+        />
+      </Button>
     </View>
   );
 }
